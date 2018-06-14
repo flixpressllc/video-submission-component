b67c6a92c1679832adfa8d51efa0f64e19b2b0a2
Don Denton <don@happycollision.com>
2018-06-14 13:27:47 -0500

    change to a function for easier switching based on env

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,9 +7,14 @@ const commonjs = require('rollup-plugin-commonjs');
 module.exports = function(defaults) {
   let app = new GlimmerApp(defaults, {
     inlineContent: {
-      'upload-url': { content: '//localhost:50212/api/upload' },
+      'upload-url': { content: (function() {
+        switch(process.env.EMBER_ENV) {
+          default:
+          return '//localhost:50212/api/upload';
+        }
+      })() },
     },
-    
+
     rollup: {
       plugins: [
         resolve({ jsnext: true, module: true, main: true }),

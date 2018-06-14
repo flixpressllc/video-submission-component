b35e46158f7cd04816e9836a396eddb3942ce813
Don Denton <don@happycollision.com>
2018-06-14 12:38:13 -0500

    Update to upload all at once
    Izzy figured out how to accept multiple pieces of data along with an upload

--- src/ui/components/DataUploader/component.ts
+++ src/ui/components/DataUploader/component.ts
@@ -3,7 +3,6 @@ import i18n from '../i18n/helper';
 
 interface IArgs {
   uploadUrl: string;
-  submitUrl: string;
   file: File;
   name: string;
   email: string;
@@ -14,24 +13,19 @@ export default class DataUploader extends Component {
   @tracked private message = i18n('message.uploading');
 
   public didInsertElement() {
-    this.uploadVideo()
-    .then(this.submitData)
+    this.uploadVideoAndData()
     .then(this.args.onComplete)
     .catch(() => {
       this.message = i18n('message.upload_failure');
     });
   }
 
-  private uploadVideo() {
-    const {uploadUrl, file}: IArgs = this.args;
+  private uploadVideoAndData() {
+    const {uploadUrl, file, name: firstName, email}: IArgs = this.args;
     const formdata = new FormData();
-    formdata.append('video', file);
+    const data = JSON.stringify({email, firstName});
+    formdata.append('files', file);
+    formdata.append('submission', data);
     return window.fetch(uploadUrl, { method: 'POST', body: formdata });
-  }
-
-  private async submitData(uploadResponse: Response) {
-    const {name, email, submitUrl}: IArgs = this.args;
-    const videoUrl = (await uploadResponse.json()).videoUrl;
-    return window.fetch(this.args.submitUrl, { method: 'POST', body: JSON.stringify({name, email, videoUrl}) });
   }
 }

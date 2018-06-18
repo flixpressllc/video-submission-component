b0e365b7709bd8fda9d0181e5637859a03c97108
Don Denton <don@happycollision.com>
2018-06-18 18:00:40 -0500

    Add/Change i18n messages
    * add file_description
    * add file_size_warning
    * change video_finished_uploading
    * add video_finished_uploading_append_email

--- src/utils/i18n/english.ts
+++ src/utils/i18n/english.ts
@@ -12,8 +12,11 @@ export default {
     uploading: 'Uploading...',
   },
   paragraph: {
+    file_description: 'Choose a video from your phone or computer of you or you and your friends cheering.',
+    file_size_warning: 'Your video should be about 5 seconds long. Any longer and your upload will take more time than necessary. We will use about 3 seconds of your video.',
     no_email_consequences: 'If you do not provide an email address, we can not send you a link when your video is done processing.',
     no_name_consequences: 'If you do not provide your name, we cannot personalize your video.',
-    video_finished_uploading: 'Your video has uploaded!'
+    video_finished_uploading: 'Your video has uploaded. If your video is picked, you may see yourself in a commercial on KGKC Telemundo of Kansas City!',
+    video_finished_uploading_append_email: 'Either way, we will email you with a link to share your video.',
   },
 };
--- src/utils/i18n/spanish.ts
+++ src/utils/i18n/spanish.ts
@@ -12,8 +12,11 @@ export default {
     uploading: 'Cargando...',
   },
   paragraph: {
+    file_description: 'Elija un video de su teléfono o computadora suyo o de usted y de sus amigos.',
+    file_size_warning: 'Tu video debe durar unos 5 segundos. Más tiempo y su carga tomará más tiempo de lo necesario. Usaremos aproximadamente 3 segundos de tu video.',
     no_email_consequences: 'Si no proporciona una dirección de correo electrónico, no podemos enviarle un enlace cuando finalice el procesamiento de su video.',
     no_name_consequences: 'Si no proporciona su nombre, no podemos personalizar su video.',
-    video_finished_uploading: 'Tu video ha sido subido'
+    video_finished_uploading: 'Tu video ha sido subido. Si su video es elegido, ¡puede verse en un comercial en KGKC Telemundo de Kansas City!',
+    video_finished_uploading_append_email: 'De cualquier manera, le enviaremos un correo electrónico con un enlace para compartir su video.',
   },
 };

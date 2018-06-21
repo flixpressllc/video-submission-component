7893ab4bf1ecd0043db1839bd1b350b813cabc04
Don Denton <don@happycollision.com>
2018-06-21 17:30:43 -0500

    using xhr: allows getting upload percent

--- src/ui/components/DataUploader/component.ts
+++ src/ui/components/DataUploader/component.ts
@@ -26,6 +26,54 @@ export default class DataUploader extends Component {
     const data = JSON.stringify({email, firstName});
     formdata.append('files', file);
     formdata.append('submission', data);
-    return window.fetch(uploadUrl, { method: 'POST', body: formdata });
+    // return window.fetch(uploadUrl, { method: 'POST', body: formdata });
+    return new Promise((res, rej) => {
+      const oReq = new XMLHttpRequest();
+      const updateProgress = (oEvent) => {
+        if (oEvent.lengthComputable) {
+          console.log(oEvent);
+          this.uploadProgress(oEvent.loaded / oEvent.total * 100);
+          // ...
+        }
+      };
+
+      oReq.upload.addEventListener('progress', updateProgress);
+      oReq.addEventListener('load', transferComplete);
+      oReq.addEventListener('error', transferFailed);
+      oReq.addEventListener('abort', transferCanceled);
+
+      oReq.open('POST', uploadUrl);
+      oReq.send(formdata);
+
+      // ...
+
+      function transferComplete(evt) {
+        console.log('transfer complete');
+        res();
+        teardown();
+      }
+
+      function transferFailed(evt) {
+        console.log('transfer failed');
+        rej(evt);
+        teardown();
+      }
+
+      function transferCanceled(evt) {
+        rej('The transfer has been canceled by the user.');
+        teardown();
+      }
+
+      function teardown() {
+        oReq.upload.removeEventListener('progress', updateProgress);
+        oReq.removeEventListener('load', transferComplete);
+        oReq.removeEventListener('error', transferFailed);
+        oReq.removeEventListener('abort', transferCanceled);
+      }
+    });
+  }
+
+  private uploadProgress(percent: number) {
+    console.log(percent);
   }
 }
